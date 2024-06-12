cad8e1fa60415f2487f32dbaa8edb30d650d8485
Shoxrux Rasulov <shokhrukhrasulov07@gmail.com>
2024-06-12 17:03:54 +0500

    umumiy arhitektura tuzildi.

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return const ScreenUtilInit(
       designSize: Size(442, 888),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: OnbordingScreen(),
       ),
     );
