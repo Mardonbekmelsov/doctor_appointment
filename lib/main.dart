7bd87fcc5472271897fc1b74e02617d84066ccd9
Mardonbekmelsov <153820615+Mardonbekmelsov@users.noreply.github.com>
2024-06-12 14:53:40 +0500

    dd

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main(List<String> args) {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      body: Container(),
+    );
   }
 }
