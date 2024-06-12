23d343435c726dc8df89fd15b5ea9a0ce6bd8edc
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-06-12 14:36:09 +0500

    sdas

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,11 @@ void main(List<String> args) {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
